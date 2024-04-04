c777de1571e4a62aa14f4acc387a18cac1ff3059
weirdobeaver <weirdobeaver88@gmail.com>
2024-04-04 11:48:40 +0200

    Add users table

---
+++ priv/repo/migrations/20240404093053_create_users_table.exs
@@ -0,0 +1,15 @@
+defmodule SampleEx.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string, null: false
+      add :email, :string, null: false
+
+      timestamps()
+    end
+
+    create index(:users, [:name])
+    create unique_index(:users, [:email])
+  end
+end
